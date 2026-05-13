495392980017d3935a334684c49293508015f2de
FlaviaRebello <flavia.mrebello19@gmail.com>
2026-05-13 09:39:59 -0300

    Update Treino.cs

--- Academia.Api/Models/Treino.cs
+++ Academia.Api/Models/Treino.cs
@@ -1,22 +1,26 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Academia.Api.Models;
-
-public class Treino
+namespace Academia.Api.Models
 {
-    public int Id { set; get; }
-    [Required(ErrorMessage = "o nome do treino é onrigatóiro")]
-    public string? NomeTreino { set; get; }
-    [Required]
-    public int AlunoId { set; get; }
-    [ForeignKey("AlunoId")]
-    public Aluno? Aluno { set; get; }
-    [Required]
-    public int ExercicioId{ set; get; }
-    [ForeignKey("ExercicioId")]
-    public Exercicio? Exercicio { set; get; }
-    public int Series { set; get; }
-    public int Repeticoes { set; get; } 
-     public List<Exercicio>? Exercicios { get; set; } 
+    public class Treino
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório")]
+        [MaxLength(100, ErrorMessage = "Nome deve ter no máximo 100 caracteres")]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Data é obrigatória")]
+        public DateTime Data { get; set; } = DateTime.UtcNow;
+
+        [Required(ErrorMessage = "Aluno é obrigatório")]
+        public int AlunoId { get; set; }
+
+        [ForeignKey("AlunoId")]
+        public Aluno? Aluno { get; set; }
+
+        public ICollection<TreinoExercicio> TreinosExercicios { get; set; } = new List<TreinoExercicio>();
+    }
 }
